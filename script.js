f28e48c8deabceda5cb07d18d101b253086b7405
pietrolupi <pietro.lupi92@gmail.com>
2023-09-27 16:19:16 +0200

    [add]: ternario interno alla classe del messaggio

--- script.js
+++ script.js
@@ -10,14 +10,25 @@ createApp({
 
       image: ` <img class="mt-5" src="img0.jpg" alt="">`,
 
+      isOkFlag: true,
+
+      greenText: 'green'
     }
     
   },
 
   methods: {
     switchImg(){
-      this.image =  ` <img class="mt-5" src="img1.webp" alt="">`
-      this.message = '.......'
-    }
+      this.image =  ` <img class="mt-5" src="img1.webp" alt="">`;
+      this.message = '....... ah';
+      this.isOkFlag = false;
+      this.greenText = ''
+      console.log(this)
+    },
+
+  },
+
+  mounted(){
+    console.log('mounted TEST')
   }
 }).mount('#app')
