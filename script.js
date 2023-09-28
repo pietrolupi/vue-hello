11c20678d7112a5dd073a15af357a16d6704c432
pietrolupi <pietro.lupi92@gmail.com>
2023-09-28 08:37:25 +0200

    [add]: input modifica messaggio al press Invio

--- script.js
+++ script.js
@@ -12,7 +12,12 @@ createApp({
 
       isOkFlag: true,
 
-      greenText: 'green'
+      greenText: 'green',
+
+      coloreTesto2: '',
+
+      messaggioCheCambia: 'Questo messaggio cambia scrivendo qua sotto e premendo INVIO',
+      messaggioCambiato : ''
     }
     
   },
@@ -22,13 +27,27 @@ createApp({
       this.image =  ` <img class="mt-5" src="img1.webp" alt="">`;
       this.message = '....... ah';
       this.isOkFlag = false;
-      this.greenText = ''
-      console.log(this)
+      
     },
 
+
+    stampSecondMessage(){
+      const containerMessaggio = document.querySelector('.messaggio2');
+      const message2 = document.createElement('p');
+      message2.innerHTML = 'Messaggio secondario stampato tramite methods e mounted direttamente, un pochino di testo su cui sperimentare.  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet pariatur expedita mollitia ipsum aut eligendi veritatis accusantium recusandae magni quibusdam. ';
+
+      containerMessaggio.append(message2);
+      
+    },
+
+    cambioMessaggio(){
+      this.messaggioCheCambia = this.messaggioCambiato;
+    }
   },
 
   mounted(){
-    console.log('mounted TEST')
+    
+    this.stampSecondMessage();
+
   }
 }).mount('#app')
